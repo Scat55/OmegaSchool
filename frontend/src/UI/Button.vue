b8ed3ffb41e3640eaff48cb916d2570d4105d2c9
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-10-06 14:27:30 +0300

    Перенес оставшиеся файлы

---
+++ frontend/src/UI/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    :class="{ active: isActive }"
+    @click="toggleActive"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: Boolean,
+  },
+  methods: {
+    toggleActive() {
+      this.$emit("update:isActive", !this.isActive);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/styles.scss';
+
+button {
+  font-size: 0.96rem;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid #018bd3;
+  background: none;
+  border-radius: 1rem;
+  font-family: Visitor;
+}
+
+button.active {
+  background-color: #018bd3;
+  color: white;
+}</style>
