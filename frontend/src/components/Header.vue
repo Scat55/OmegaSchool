d042f3c2d16d536e22001aaf9f4fc2d9dd2f254b
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-10-06 14:34:31 +0300

    Добавил ссылку на лого в хедере

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,13 +3,13 @@
     <div class="container">
 
       <div class="header__logo">
-        <!-- <RouterLink to="/"> -->
-        <img
-          src="../assets/images/Logo_px (2).png"
-          alt="Logo"
-          class="header__logo-img"
-        >
-        <!-- </RouterLink> -->
+        <router-link to="/">
+          <img
+            src="../assets/images/Logo_px (2).png"
+            alt="Logo"
+            class="header__logo-img"
+          >
+        </router-link>
         <div class="header__logo-lists">
           <ul class="header__logo-list">
             <li
