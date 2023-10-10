9d912d514e07606a9362216d6afab9ae0229695d
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-10-10 15:24:05 +0300

    Сделал переход в лк

--- frontend/src/views/testProfile.vue
+++ frontend/src/views/testProfile.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <h1>Profile id {{ id }}</h1>
+    <h1>Profile id {{ id }} {{ email }}</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      id: this.$router.currentRoute.params['id']
+      id: this.$router.currentRoute.params['id'],
+      email: ''
     }
+  },
+  mounted() {
+    axios.get(`/getUser/${this.email}`).then(response => {
+      console.log(response.data)
+    })
   },
 }
 </script>
