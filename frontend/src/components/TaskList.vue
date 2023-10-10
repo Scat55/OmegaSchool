e506034ec08d44a435d6a81f60cff3818040ace4
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-10-10 10:29:27 +0300

    Изменил вид задач
    Думаю описание не нужно на телефоне будет много место занимать.
    Либо же позже добавить скрытую аннотацию

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -13,9 +13,12 @@ export default {
   <div class="task">
     <router-link :to="'/task/' + task.id" class="title">{{ task.title }}
     </router-link>
-    <div class="topic">Предмет: {{task.topic}}</div>
-    <div class="complexity">Уровень задания: {{task.complexity}}</div>
-    <div class="bodyTask">{{task.bodyTask}}</div>
+    <div class="body">
+      <div class="topic">Предмет: {{task.topic}}</div>
+      <div class="status">Статус: {{task.status === false ? 'Не решено' : 'Решено'}}</div>
+      <div class="complexity">Уровень задания: {{task.complexity}}</div>
+    </div>
+<!--    <div class="bodyTask">{{task.bodyTask}}</div>-->
   </div>
 </template>
 
@@ -27,11 +30,21 @@ export default {
   border: 2px solid $lightBlueColor;
   border-radius: 1rem;
   margin-bottom: 15px;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: repeat(3, 1fr);
+  display: flex;
+  flex-direction: column;
+  //display: grid;
+  //grid-template-columns: 1fr 1fr;
+  //grid-template-rows: repeat(3, 1fr);
   background: white;
   overflow: hidden;
+}
+
+.body {
+  margin: 10px ;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 5px 0;
 }
 
 .task:hover {
@@ -39,33 +52,31 @@ export default {
 }
 
 .title {
-  padding-top: 15px;
-  grid-column: 1 / span 4;
-  grid-row: 1 / span 1;
+  padding: 10px 0;
+  //grid-column: 1 / span 4;
+  //grid-row: 1 / span 1;
   font-size: 20px;
-  border-bottom: 1px solid $lightBlueColor;
+  //border-bottom: 1px solid $lightBlueColor;
   background: $lightBlueColor;
 }
 
 
 .topic {
-  grid-column: 1 / span 1;
-  grid-row: 2 / span 1;
+  //grid-column: 1 / span 1;
+  //grid-row: 2 / span 1;
   text-align: left;
-  margin-left: 25px;
 }
 
 .complexity {
-  grid-column: 2 / span 1;
-  grid-row: 2 / span 1;
-  margin-right: 20px;
+  //grid-column: 2 / span 1;
+  //grid-row: 2 / span 1;
   text-align: right;
 }
 
-.bodyTask {
-  grid-column: 1 / span 4;
-  grid-row: 3 / span 1;
-  text-align: left;
-  margin: 0 20px 10px 20px;
-}
+//.bodyTask {
+  //grid-column: 1 / span 4;
+  //grid-row: 3 / span 1;
+  //text-align: left;
+  //margin: 0 20px 10px 20px;
+//}
 </style>
