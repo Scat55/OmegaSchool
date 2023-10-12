9ea9395280c3a7bc7003baf2cf00bd6352559c4b
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-10-12 12:36:52 +0300

    TaskDetail стили

--- frontend/src/components/TaskDetail.vue
+++ frontend/src/components/TaskDetail.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="task-detail">
-    <h2>{{ task.title }}</h2>
-    <h2>{{ $route.params.id }}</h2>
-    <div class="topic">Предмет: {{ task.topic }}</div>
-    <div class="complexity">Уровень задания: {{ task.complexity }}</div>
-    <div class="bodyTask">{{ task.bodyTask }}</div>
+  <div class="container">
+    <div class="container__title">
+      <h2>{{ task.title }}</h2>
+    </div>
+    <div class="container__infoTask">
+      <div class="container__infoTask__id">
+        <p>Номер задачи: {{ task.id }}</p>
+      </div>
+      <div class="container__infoTask__complexity">
+        Уровень задания: {{ task.complexity }}
+      </div>
+      <div class="container__infoTask__topic">
+        Предмет: {{ task.topic }}
+      </div>
+    </div>
+    <div class="container__bodyTask">
+      <span>Условие задания:</span> {{ task.bodyTask }}
+    </div>
+    <div class="container_answer">
+<!--      <li v-for="item in 8">-->
+<!--        <input type="checkbox" name="" id="" ><label>{{item}}</label>-->
+<!--      </li>-->
+    </div>
+    <div class="container_button">
+
+    </div>
   </div>
 </template>
 
@@ -24,4 +44,37 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  margin-top: 80px;
+  background: white;
+  padding: 10px;
+  border-radius: 1rem;
+  border: 2px solid #487fb8;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__infoTask {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin: 10px 0;
+
+    &__id {
+
+    }
+
+  }
+
+  &__bodyTask {
+
+    &>span{
+      font-weight: bold;
+    }
+
+  }
+
+}
+</style>
