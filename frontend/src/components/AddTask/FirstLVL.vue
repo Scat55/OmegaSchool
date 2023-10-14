8c81a0fb8f41dea8d99c242f22f6c8e6ec072648
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-10-14 14:29:45 +0300

    Добавил стили чекбоксу.

--- frontend/src/components/AddTask/FirstLVL.vue
+++ frontend/src/components/AddTask/FirstLVL.vue
@@ -3,8 +3,8 @@ export default {
   data() {
     return {
       checkboxes: [
-        { text: '', checked: false},
-        { text: '', checked: false}
+        {text: '', checked: false},
+        {text: '', checked: false}
       ],
       // checkboxText: Array(2).fill(''),
       isWindowOpen: false,
@@ -12,7 +12,7 @@ export default {
   },
   methods: {
     addCheckbox() {
-      this.checkboxes.push({ text: '', checked: false});
+      this.checkboxes.push({text: '', checked: false});
     },
     toggleWindow() {
       this.isWindowOpen = !this.isWindowOpen;
@@ -23,14 +23,14 @@ export default {
     sendTest() {
       console.log(this.checkboxes)
     },
-    addTask(){
+    addTask() {
       alert('Заданме добавлено')
     },
 
     deleteCheckBox() {
       this.checkboxes = [
-        { text: '', checked: false},
-        { text: '', checked: false}
+        {text: '', checked: false},
+        {text: '', checked: false}
       ]
     }
   },
@@ -50,28 +50,32 @@ export default {
         <textarea></textarea>
       </div>
       <div class="shablonZadaniaFirst__window">
-        <button @click="toggleWindow" type="button">{{ isWindowOpen === false ? 'Показать инструкцию' : 'Скрыть инструкцию' }}</button>
+        <button @click="toggleWindow" type="button">
+          {{ isWindowOpen === false ? 'Показать инструкцию' : 'Скрыть инструкцию' }}
+        </button>
         <div v-if="isWindowOpen">
           <p>Тут будет инструкци как использовать чекбоксы. Нажать на кнопку "добавить чекбокс" добавит один чекбокс в
             инпут надо будет ввести значение. Те которые правильные варианты ответа нужно будет ответить нажам на
             чекбокс</p>
         </div>
       </div>
-<!--  Вот здесь начинается блок с checkbox    -->
+      <!--  Вот здесь начинается блок с checkbox    -->
       <div class="shablonZadaniaFirst__option_answers">
         <button @click="addCheckbox" type="button" v-if="checkboxes.length !== 6">Добавить чекбокс</button>
         <div v-for="(checkbox, index) in checkboxes" :key="index">
-          <div class="shablonZadaniaFirst__checkbox-item">
-            <input type="checkbox" v-model="checkbox.checked" :id="'checkbox-' + index"/>
-            <input type="text" v-model="checkbox.text"/>
+          <div class="shablonZadaniaFirst__checkbox_item">
+            <label>{{ index + 1 }})&nbsp;</label>
+            <input class="shablonZadaniaFirst__checkbox_item_check" type="checkbox" v-model="checkbox.checked"
+                   :id="'checkbox-' + index"/>
+            <input class="shablonZadaniaFirst__checkbox_item_input" type="text" v-model="checkbox.text"/>
             <button @click="removeCheckbox(index)" type="button">X</button>
           </div>
         </div>
       </div>
-<!--   Конец. Наконец-то сделал адекватный вывод. Я бухать!   -->
+      <!--   Конец. Наконец-то сделал адекватный вывод. Я бухать!   -->
       <div class="shablonZadaniaFirst__btn_send">
-        <button  @click="sendTest" class = "btn" type="submit">Отправить задание на проверку эксперту!</button>
-        <button  class = "btn-reset" type="reset" @click="deleteCheckBox">Удалить все!</button>
+        <button @click="sendTest" class="btn" type="submit">Отправить задание на проверку эксперту!</button>
+        <button class="btn-reset" type="reset" @click="deleteCheckBox">Сброс</button>
       </div>
     </div>
   </form>
@@ -80,6 +84,7 @@ export default {
 
 <style scoped lang="scss">
 @import '../../assets/styles/vars';
+
 .shablonZadaniaFirst {
 
   display: flex;
@@ -100,6 +105,7 @@ export default {
       border-radius: 1rem;
       border: none;
       padding: 8px;
+      outline: none
     }
   }
 
@@ -118,6 +124,7 @@ export default {
       margin-bottom: 10px;
       padding: 8px;
       border-radius: 1rem;
+      outline: none
     }
   }
 
@@ -156,7 +163,34 @@ export default {
     }
   }
 
-  &__checkbox-item {
+  &__checkbox_item {
+
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+
+    &_check {
+      margin-right: 3px;
+
+    }
+
+    &_input {
+      border: none;
+      margin-right: 3px;
+      border-radius: 4px;
+      &:focus {
+        border: none;
+        outline: none
+      }
+    }
+
+    & > button {
+      padding: 2px;
+      background: #ff6e6e;
+      color: white;
+      border-radius: 4px;
+      border: none;
+    }
 
   }
 
@@ -171,7 +205,7 @@ export default {
 
 }
 
-.btn{
+.btn {
   padding: 8px;
   border-radius: 1rem;
   outline: none;
@@ -179,10 +213,11 @@ export default {
   cursor: pointer;
   transition: all .3s;
 
-  &:hover{
+  &:hover {
     background-color: #00DFCC;
   }
 }
+
 .btn-reset {
   padding: 8px;
   border-radius: 1rem;
