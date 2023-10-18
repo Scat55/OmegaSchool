6f4746e178eac9d3f0efc0e7340a12c1586ce413
nikita_siniukov <siniukovnikita@gmail.com>
2023-10-18 13:29:35 +0300

    add file

--- newBack/routes/user.routes.js
+++ newBack/routes/user.routes.js
@@ -10,21 +10,19 @@ const userMiddleware = require('../middlewaree/userMiddlewaer');
 const roleMiddleware = require('../middlewaree/roleMiddlewaer')
 
 
-<<<<<<< HEAD
 router.get('/user_list', roleMiddleware(['Ученик','Эксперт']), userController.getUserList)
 router.get('/user_id/:email',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserIDForEmail)
 router.get('/user_inf_email/:email',userMiddleware, userController.getUserDataForEmail)
 router.get('/user_inf/:user_id',userMiddleware, userController.getUserInformation)
 router.post('/addition_data',userMiddleware, userController.additionalData)
 router.get('/addition_data',userMiddleware, userController.additionalData)
-=======
+
 
 router.get('/user_list', roleMiddleware(['Ученик','Эксперт']), userController.getUserList)
 router.get('/user_id/:email',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserIDForEmail)
 router.get('/user_inf_email/:email',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserDataForEmail)
 router.get('/user_inf/:user_id',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserInformation)
 router.post('/addition_data',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.additionalData)
->>>>>>> origin/main
 
 router.post('/upload/',
     // roleMiddleware(['Эксперт','Учитель']),
