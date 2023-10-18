a594e56b6014dc1316e513438bfc367122266b79
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2023-10-18 13:58:54 +0300

    подправил бэк

--- newBack/routes/user.routes.js
+++ newBack/routes/user.routes.js
@@ -15,14 +15,6 @@ router.get('/user_id/:email',roleMiddleware(['Ученик','Эксперт','У
 router.get('/user_inf_email/:email',userMiddleware, userController.getUserDataForEmail)
 router.get('/user_inf/:user_id',userMiddleware, userController.getUserInformation)
 router.post('/addition_data',userMiddleware, userController.additionalData)
-router.get('/addition_data',userMiddleware, userController.additionalData)
-
-
-router.get('/user_list', roleMiddleware(['Ученик','Эксперт']), userController.getUserList)
-router.get('/user_id/:email',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserIDForEmail)
-router.get('/user_inf_email/:email',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserDataForEmail)
-router.get('/user_inf/:user_id',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.getUserInformation)
-router.post('/addition_data',roleMiddleware(['Ученик','Эксперт','Учитель']), userController.additionalData)
 
 router.post('/upload/', userMiddleware, userController.postFile)
 router.get('/download', roleMiddleware(['Эксперт','Учитель']), userController.getFile)
