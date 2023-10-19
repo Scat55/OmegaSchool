974b4d161c39d47aa283fc2ecfeaf64072338659
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-10-19 12:55:58 +0300

    Поправил дату и сделал условие для экспертов

--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -150,7 +150,8 @@ export default {
       this.person.patronymic = response.data.user.patronymic
       this.person.gender = response.data.user.gender
       this.person.item = response.data.user.item
-      this.person.birthday = format(new Date(response.data.user.birthdata), 'dd.MM.yyyy') // Приводим дату в человеческий вид с помощью data-fns
+      this.person.expert = response.data.user.expert === 'Да'
+      this.person.birthday = format(new Date(response.data.user.birthdate), 'dd.MM.yyyy') // Приводим дату в человеческий вид с помощью data-fns
       if (response.data.user.expert === "true") {
         this.person.expert = true
       }
