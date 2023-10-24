6d24269b74be48f93742ab440ef1d81221ed2fcd
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-10-24 15:14:10 +0300

    Поменял стили у кнопки

--- frontend/src/views/TaskPage.vue
+++ frontend/src/views/TaskPage.vue
@@ -139,7 +139,7 @@ export default {
     },
 
     paginatedTasks() {
-      const start = (this.currentPage -1 ) * this.tasksPerPage;
+      const start = (this.currentPage - 1) * this.tasksPerPage;
       const end = start + this.tasksPerPage;
       return this.filteredTasks.slice(start, end);
     },
@@ -372,12 +372,13 @@ export default {
       cursor: pointer;
     }
 
-    &__hide {
-      background: $lightBlueColor;
-    }
-
     &__reset {
-      background: red;
+      background: $lightBlueColor;
+      transition: all .3s;
+
+      &:hover{
+        transform: scale(1.1);
+      }
     }
   }
 
