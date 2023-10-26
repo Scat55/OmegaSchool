44acf269bb1289addc4ad86236921252b0319114
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-10-26 14:48:13 +0300

    Сднлал отображение страницы по id

--- frontend/src/views/TaskToExpert.vue
+++ frontend/src/views/TaskToExpert.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>kdfksdfndnf</div>
+  <div class="container">
+    <div class="task__info">fgsdfgdfg</div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '../assets/styles/vars.scss';
+.task__info {
+  border: 2px solid $lightBlueColor;
+  border-radius: 1rem;
+  margin-top: 2rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  overflow: hidden;
+  transition: all 0.3s;
+}
+</style>
