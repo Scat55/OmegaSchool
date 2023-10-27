9355773282ccebf3745776af7f7521362cacb9a0
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-10-27 10:46:34 +0300

    Стилизовал задачу

--- frontend/src/views/TaskToExpert.vue
+++ frontend/src/views/TaskToExpert.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
     <div class="task__info">
-      {{ id }},
-      {{ info.test_id }}
+      <p>{{ info.test_text }}</p>
+      <p>{{ info.test_description }}</p>
+      <div v-for="question in info.questions" class="options">
+        <div v-for="option in question.options">{{ option.text }} - {{ option.is_correct }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +38,10 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/styles/vars.scss';
 .task__info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
   border: 2px solid $lightBlueColor;
   border-radius: 1rem;
   margin-top: 2rem;
@@ -44,5 +51,11 @@ export default {
   background: white;
   overflow: hidden;
   transition: all 0.3s;
+}
+.options {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2rem;
 }
 </style>
