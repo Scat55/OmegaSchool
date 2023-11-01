bb46ae7b2838d33e944cc6242ecc238c0058863c
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-01 14:29:16 +0300

    Готовое отображение всех задач учителя

--- frontend/src/components/MyAddTask/AddTaskChecked.vue
+++ frontend/src/components/MyAddTask/AddTaskChecked.vue
@@ -1,9 +1,8 @@
 <!-- Тут компанент который выводит все эти задачи которые добавил учитель -->
 <template>
   <div class="taskTeacher">
-    <h1>{{ task.id }}</h1>
-    <h1>{{ task.title }}</h1>
-    <h1>{{ task.statusCheck }}</h1>
+    <p class="taskTeacher__name">{{ task.task_test }}</p>
+    <span>Уровень - {{ task.level }}</span>
   </div>
 </template>
 
@@ -18,10 +17,22 @@ export default {
 
 <style scoped lang="scss">
 .taskTeacher {
+  display: flex;
+  justify-content: space-between;
   background-color: #fff;
   margin-top: 1rem;
   border: 1px solid #000;
   border-radius: 1rem;
   padding: 0.625rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    transform: scale(1.03);
+  }
+
+  &__name {
+    font-size: 1.1rem;
+  }
 }
 </style>
