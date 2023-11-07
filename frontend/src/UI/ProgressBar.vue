00ec96d67ee58ed563cdcc32f425a147dea11d06
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-11-07 22:35:05 +0300

    Че то сделал

---
+++ frontend/src/UI/ProgressBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="progress-container">
+      <div class="progress-bar" :style="progressBarStyle"></div>
+      <div :class="[this.score >= 50 ? 'progress-text_white' : '', 'progress-text']">{{ progressText }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      score: {
+        type: Number,
+        required: true
+      }
+    },
+    computed: {
+      progressBarStyle() {
+        const width = this.score > 100 ? 100 : this.score;
+        return {
+          width: width + '%',
+          backgroundColor: 'blue',
+          height: '20px',
+          transition: 'width 0.5s ease',
+        };
+      },
+      progressText() {
+        return this.score > 100 ? '100%' : this.score + '%';
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .progress-container {
+    width: 100%;
+    background-color: #eee;
+    border-radius: 4px;
+    overflow: hidden;
+    position: relative;
+  }
+  
+  .progress-bar {
+    border-radius: 4px;
+  }
+  
+  .progress-text {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 20px;
+    font-weight: bold;
+  }
+
+  .progress-text_white {
+    color: white;
+  }
+  </style>
+  
--- frontend/src/components/Profile/RatingTeacher.vue
+++ frontend/src/components/Profile/RatingTeacher.vue
@@ -1,5 +1,9 @@
 <script>
+import ProgressBar from '@/UI/ProgressBar.vue';
 export default {
+  components: {
+    ProgressBar
+  },
   props: {
     grades: {
       type: Array,
@@ -21,24 +25,41 @@ export default {
     <div class="grades_teacher informatics">
       <h3>Мой балл по информатике: {{ grades[0].informatics }}</h3>
       <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].informatics"></progress-bar>
     </div>
     <div class="grades_teacher mathematics">
       <h3>Мой балл по математике: {{ grades[0].mathematics }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].mathematics"></progress-bar>
     </div>
     <div class="grades_teacher physics">
       <h3>Мой балл по физике: {{ grades[0].physics }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].physics"></progress-bar>
     </div>
     <div class="grades_teacher chemistry">
       <h3>Мой балл по химии: {{ grades[0].chemistry }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].chemistry"></progress-bar>
     </div>
     <div class="grades_teacher biology">
       <h3>Мой балл по биологии: {{ grades[0].biology }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].biology"></progress-bar>
     </div>
     <div class="grades_teacher geography">
       <h3>Мой балл по географии: {{ grades[0].geography }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].geography"></progress-bar>
     </div>
     <div class="grades_teacher technology">
       <h3>Мой балл по технологии: {{ grades[0].technology }}</h3>
+      <p>Мой прогресс по достижение звания "Эксперт" в этом предмете:</p>
+      <progress-bar :score="grades[0].technology"></progress-bar>
+    </div>
+
+    <div class="my_achivment">
+      
     </div>
 </div>
 </template>
