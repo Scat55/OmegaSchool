5902dfcfddf26d7cf82f6d121bbe02a9b70e243e
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2023-11-07 10:00:03 +0300

    поправил мидлвеар

--- newBack/routes/user.routes.js
+++ newBack/routes/user.routes.js
@@ -39,8 +39,9 @@ router.post('/updateTestByExpert',roleMiddleware(['Эксперт']), userContro
 
 
 router.get('/getTasksForTeacherByID/:testID',roleMiddleware(['Учитель']), userController.getTasksByID)
-router.get('/getTasksForTeacher',roleMiddleware(['Учитель']), userController.getTasksForTeacher)
+router.get('/getTasksForTeacher',roleMiddleware(['Учитель','Эксперт']), userController.getTasksForTeacher)
 router.get('/getTasksForTeacherByStudent',roleMiddleware(['Учитель']), userController.getTasksForTeacherByStudent)
+//router.get('/getTasksForTeacherByStudentByID/:testID',roleMiddleware(['Учитель']), userController.getTasksForTeacherByStudentByID)
 //
 //
 //
