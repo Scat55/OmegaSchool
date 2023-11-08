bde754c4ecf9010223c0753d053442b862fef2ec
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-11-08 09:55:16 +0300

    Сделал шаблон

--- frontend/src/components/Profile/RatingTeacher.vue
+++ frontend/src/components/Profile/RatingTeacher.vue
@@ -58,8 +58,34 @@ export default {
       <progress-bar :score="grades[0].technology"></progress-bar>
     </div>
 
+
+    <h2 class="title_my_rating">Мои достижения</h2>
     <div class="my_achivment">
-      
+      <div class="achievement-card">
+        <img src="@/assets/images/prizes/prizes (1).png" alt="achievement.title" class="achievement-image">
+        <h4 class="achievement-title">Первое задание создано!</h4>
+        <p class="achievement-text">Ваш первый вклад в обучение неоценим. Продолжайте вдохновлять учеников!</p>
+      </div>
+      <div class="achievement-card">
+        <img src="@/assets/images/prizes/prizes (18).png" alt="achievement.title" class="achievement-image">
+        <h4 class="achievement-title">25 заданий в арсенале!</h4>
+        <p class="achievement-text">Вы создали 25 уникальных заданий. Ваше мастерство формирует будущее образования!</p>
+      </div>
+      <div class="achievement-card">
+        <img src="@/assets/images/prizes/prizes (5).png" alt="achievement.title" class="achievement-image">
+        <h4 class="achievement-title">50 заданий для жаждущих знаний!</h4>
+        <p class="achievement-text">50 заданий добавлено! Ваше стремление к обучению укрепляет основы знаний.</p>
+      </div>
+      <div class="achievement-card">
+        <img src="@/assets/images/prizes/prizes (4).png" alt="achievement.title" class="achievement-image">
+        <h4 class="achievement-title">75 шедевров педагогики!</h4>
+        <p class="achievement-text">Вы достигли великолепной отметки в 75 заданий! Ваш опыт и творчество ценятся выше всего.</p>
+      </div>
+      <div class="achievement-card inactive">
+        <img src="@/assets/images/prizes/prizes (6).png" alt="achievement.title" class="achievement-image">
+        <h4 class="achievement-title">100 заданий - вековой юбилей!</h4>
+        <p class="achievement-text">Ваши 100 заданий стали вехой в образовании. Вы истинная легенда учения!</p>
+      </div>
     </div>
 </div>
 </template>
@@ -91,4 +117,70 @@ export default {
     margin-bottom: 10px;
   }
 }
+
+// card
+
+.my_achivment {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px; // Определяем пространство между карточками
+
+  .achievement-card {
+    flex: 1 1 calc(33.333% - 20px); // каждая карточка занимает 1/3 ширины контейнера минус gap
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    background: #fff;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    opacity: 1;
+    &.inactive {
+      opacity: 0.5; // Полупрозрачность для неактивных карточек
+      pointer-events: none; // Отключение взаимодействия с пользователем
+    }
+
+    &:not(:nth-child(3n)) {
+      margin-right: 0; // Убираем margin справа для каждой третьей карточки
+    }
+
+    .achievement-image {
+      width: 100%; // или фиксированный размер, если нужно
+      height: auto; // сохранить пропорции изображения
+      margin-bottom: 15px;
+    }
+
+    .achievement-title {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 10px;
+      text-align: center;
+    }
+
+    .achievement-text {
+      font-size: 14px;
+      font-weight: 400;
+      text-align: center;
+    }
+  }
+}
+
+@media (max-width: 767px) { // Медиа-запрос для мобильных устройств
+  .my_achivment {
+    justify-content: center; // Центрируем карточки на маленьких экранах
+  }
+
+  .achievement-card {
+    flex-basis: 100%; // Карточки занимают всю ширину на маленьких экранах
+    max-width: 100%; // Ограничиваем максимальную ширину для учета отступов
+    opacity: 1;
+
+    &.inactive {
+      opacity: 0.1; // Полупрозрачность для неактивных карточек
+      pointer-events: none; // Отключение взаимодействия с пользователем
+    }
+  }
+}
+
 </style>
