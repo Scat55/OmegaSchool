e42994501e18f820d0beda6f82a5c8a0426a3263
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-14 10:49:18 +0300

    Поменял конфиг vue

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('pdf')
+      .test(/\.pdf$/)
+    // .use('file-loader')
+    // .loader('file-loader')
+  },
   devServer: {
     proxy: 'http://localhost:8070',
   },
