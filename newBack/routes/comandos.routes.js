7e57e13f7782d47f828cfb4f4a8570c1206e4319
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-21 11:22:44 +0300

    Поправил пути

--- newBack/routes/comandos.routes.js
+++ newBack/routes/comandos.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 //const authMiddleware = require('../middlewaree/authMiddleware');
 const commandosController = require('../controller/comandos_controller')
 const db = require("../db");
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 router.post('/create', commandosController.CreateComandos)
-router.post('/login', commandosController.loginComac)
+// router.post('/login', commandosController.loginComac)
 
 module.exports = router;
