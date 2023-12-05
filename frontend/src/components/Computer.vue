857f094ddbf1e4e9d136484c792358b94ed49f28
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-05 12:00:57 +0300

    Убрал ссылку "Смотреть задания"

--- frontend/src/components/Computer.vue
+++ frontend/src/components/Computer.vue
@@ -1,32 +1,22 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <div class="computer">
     <div class="container">
-
       <div class="computer__content">
-        <img
-          src="../assets/images/PIC_index/comp.png"
-          alt="Computer"
-          class="computer__img"
-        >
-        <p class="computer__title">Дистанционное обучение открывает перед вами новые горизонты. Благодаря совместной работе учителей и экспертов образовательного сообщества,
-          вы можете учиться у лучших специалистов, не выходя из дома. Доступ к знаниям 24/7 в любой точке
-          мира.<span class="computer__subtitle">Смотреть
-            задания &#8594</span></p>
-
+        <img src="../assets/images/PIC_index/comp.png" alt="Computer" class="computer__img" />
+        <p class="computer__title">
+          Дистанционное обучение открывает перед вами новые горизонты. Благодаря совместной работе
+          учителей и экспертов образовательного сообщества, вы можете учиться у лучших специалистов,
+          не выходя из дома. Доступ к знаниям 24/7 в любой точке мира.
+        </p>
       </div>
     </div>
   </div>
 </template>
 
-
-
 <style lang="scss" scoped>
 .computer {
-
   margin-top: 3.125rem;
 
   &__content {
@@ -51,10 +41,10 @@
     padding-top: 1rem;
     font-size: 1rem;
     cursor: pointer;
-    transition: all .3s;
+    transition: all 0.3s;
 
     &:hover {
-      transform: translateX(.625rem);
+      transform: translateX(0.625rem);
     }
   }
 }
