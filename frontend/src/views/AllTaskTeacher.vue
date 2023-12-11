fbe025e607b8dafc5a61a4b29592f382d8dacead
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-12-11 14:51:28 +0300

    Теперь name task не ебашит в одну строку.

--- frontend/src/views/AllTaskTeacher.vue
+++ frontend/src/views/AllTaskTeacher.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="infoTask">
       <div class="infoTask__titles">
-        <p>{{ infoTask.test_text }}</p>
+        <div class="infoTask__name">{{ infoTask.test_text }}</div>
         <div class="infoTask__allInfo">
           <p>Класс - {{ infoTask.classes }}</p>
           <p>Предмет - {{ infoTask.subject }}</p>
@@ -14,37 +14,19 @@
         disabled="disabled"
         class="infoTask__descr"
       ></textarea>
-      <div
-        class="images"
-        id="gallery"
-        v-if="infoTask.add_img"
-      >
+      <div class="images" id="gallery" v-if="infoTask.add_img">
         <div v-for="img in images">
-          <img
-            :src="img"
-            class="image"
-            alt="Image"
-            data-fancybox="gallery"
-          />
+          <img :src="img" class="image" alt="Image" data-fancybox="gallery" />
         </div>
       </div>
-      <div
-        v-for="question in infoTask.questions"
-        class="infoTask__options"
-      >
-        <div v-for="option in question.options">{{ option.text }} - {{
-          option.is_correct }}</div>
+      <div v-for="question in infoTask.questions" class="infoTask__options">
+        <div v-for="option in question.options">{{ option.text }} - {{ option.is_correct }}</div>
       </div>
-      <p
-        class="infoTask__show"
-        @click="isShowAnswer = !isShowAnswer"
-        v-if="isShowAnswer"
-      >Показать подсказки</p>
+      <p class="infoTask__show" @click="isShowAnswer = !isShowAnswer" v-if="isShowAnswer">
+        Показать подсказки
+      </p>
       <div v-if="isShowAnswer">
-        <div
-          class="infoTask__answer"
-          v-if="infoTask.task_hint || info.task_answer"
-        >
+        <div class="infoTask__answer" v-if="infoTask.task_hint || info.task_answer">
           <p>Подсказдка - {{ infoTask.task_hint }}</p>
           <p>Ответ - {{ infoTask.task_answer }}</p>
         </div>
@@ -64,28 +46,30 @@
         <button
           @click="downloadFiles()"
           class="infoTask__btn"
-          v-if="infoTask.add_file !== null || infoTask.add_file !==
-            ''"
-        >Скачать</button>
+          v-if="infoTask.add_file !== null || infoTask.add_file !== ''"
+        >
+          Скачать
+        </button>
         <p v-else>Файлов нет</p>
       </div>
       <div class="questions">
-        <div v-for="question in infoTask.questions"> {{ question.text
-        }} - <span v-if="question.is_correct === true">Верно</span> <span v-else>Не верно</span>
+        <div v-for="question in infoTask.questions">
+          {{ question.text }} - <span v-if="question.is_correct === true">Верно</span>
+          <span v-else>Не верно</span>
         </div>
       </div>
       <div class="mesageExpert">
         <div class="firstExpert">
           <p>Оценка первого эксперта: {{ infoTask.ver_1 }}</p>
           <div class="flex__mess">
-            <p>Сообщение: </p>
+            <p>Сообщение:</p>
             <span>{{ infoTask.ver_1_masseg }}</span>
           </div>
         </div>
         <div class="secondExpert">
           <p>Оценка второго эксперта: {{ infoTask.ver_2 }}</p>
           <div class="flex__mess">
-            <p>Сообщение: </p>
+            <p>Сообщение:</p>
             <span>{{ infoTask.ver_2_masseg }}</span>
           </div>
         </div>
@@ -116,13 +100,13 @@ export default {
       mass: '',
       images: [],
       image: '',
-      teachrID: ''
+      teachrID: '',
     };
   },
   computed: {
     splitFiles() {
-      return this.image.split(',')
-    }
+      return this.image.split(',');
+    },
   },
   methods: {
     // Скачивание файла
@@ -137,27 +121,25 @@ export default {
           'Custom-UUID': this.userID,
         },
       })
-        .then(res => res.blob())
-        .then(data => {
-          let url = URL.createObjectURL(data) // Создаем ссылку
+        .then((res) => res.blob())
+        .then((data) => {
+          let url = URL.createObjectURL(data); // Создаем ссылку
 
-          let anchor = document.createElement('a')
+          let anchor = document.createElement('a');
           anchor.href = url;
-          anchor.download = this.fileName
-          document.body.append(anchor)
-          anchor.style = "display:none"
-          anchor.click()
-          anchor.remove()
-          URL.revokeObjectURL(url)
-        })
+          anchor.download = this.fileName;
+          document.body.append(anchor);
+          anchor.style = 'display:none';
+          anchor.click();
+          anchor.remove();
+          URL.revokeObjectURL(url);
+        });
     },
   },
   created() {
     // this.initializeUserChecks();
     // this.initializeUserAnswers();
     this.token = JSON.parse(localStorage.getItem('local'));
-
-
   },
 
   mounted() {
@@ -174,20 +156,20 @@ export default {
         this.testID = response.data.test_id;
         this.addIMG = response.data.add_img;
         this.userID = response.data.user_id;
-        let nameImage = this.addIMG.split(',')
+        let nameImage = this.addIMG.split(',');
 
         for (let i = 0; i < nameImage.length; i++) {
-          axios.get(`/api/download_image/${nameImage[i]}`, {
-            headers: {
-              Authorization: `Bearer ${this.token.token}`,
-              'Custom-UUID': this.teachrID,
-            },
-          }).then(res => {
-            this.image = `data:${res.data.contentType};base64,${res.data.data}`
-            this.images.push(this.image)
-
-
-          })
+          axios
+            .get(`/api/download_image/${nameImage[i]}`, {
+              headers: {
+                Authorization: `Bearer ${this.token.token}`,
+                'Custom-UUID': this.teachrID,
+              },
+            })
+            .then((res) => {
+              this.image = `data:${res.data.contentType};base64,${res.data.data}`;
+              this.images.push(this.image);
+            });
         }
       });
 
@@ -208,10 +190,14 @@ export default {
   border: 1px solid #000;
   user-select: none;
 
-
   &__titles {
     display: flex;
     justify-content: space-between;
+  }
+
+  &__name {
+    width: 70%;
+    word-break: break-all;
   }
 
   &__allInfo {
@@ -322,7 +308,7 @@ export default {
   gap: 1rem;
   margin-top: 2rem;
 
-  &>span {
+  & > span {
     line-height: 140%;
   }
 }
@@ -330,7 +316,7 @@ export default {
 .firstExpert {
   border: 1px solid #000;
   border-radius: 0.5rem;
-  padding: .625rem;
+  padding: 0.625rem;
 
   p {
     line-height: 140%;
@@ -340,7 +326,7 @@ export default {
 .secondExpert {
   border: 1px solid #000;
   border-radius: 0.5rem;
-  padding: .625rem;
+  padding: 0.625rem;
 
   p {
     line-height: 140%;
