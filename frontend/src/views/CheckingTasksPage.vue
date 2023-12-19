0d07aab1771d65deb202acea5b97151ac8de11b2
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-19 11:41:36 +0300

    Поменял запрос на скачивание файла

--- frontend/src/views/CheckingTasksPage.vue
+++ frontend/src/views/CheckingTasksPage.vue
@@ -108,30 +108,27 @@ export default {
   methods: {
     // Скачивание файла
     async downloadFiles() {
+      console.log(this.info);
       this.token = JSON.parse(localStorage.getItem('local'));
-      await axios
-        .get(`/api/download/${this.info.add_file}`, {
-          responseType: 'blob',
-          headers: {
-            Authorization: `Bearer ${this.token.token}`,
-            'Custom-UUID': this.userID,
-          },
-        })
-        .then((response) => {
-          if (response.data.type == 'application/zip') {
-            this.blob = new Blob([response.data], { type: 'application/zip' });
-            this.url = URL.createObjectURL(this.blob);
-            const a = document.querySelector('.downloadLink');
-            a.href = this.url;
-            a.download = this.fileName;
-          }
-          if (response.data.type == 'application/pdf') {
-            this.blob = new Blob([response.data], { type: 'application/pdf' });
-            this.url = URL.createObjectURL(this.blob);
-            const a = document.querySelector('.downloadLink');
-            a.href = this.url;
-            a.download = this.fileName;
-          }
+      await fetch(`/api/download_file/${this.info.add_file_by_student}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.token.token}`,
+          'Custom-UUID': this.userID,
+        },
+      })
+        .then((res) => res.blob())
+        .then((data) => {
+          let url = URL.createObjectURL(data); // Создаем ссылку
+
+          let anchor = document.createElement('a');
+          anchor.href = url;
+          anchor.download = this.fileName;
+          document.body.append(anchor);
+          anchor.style = 'display:none';
+          anchor.click();
+          anchor.remove();
+          URL.revokeObjectURL(url);
         });
     },
     // Появление файла
