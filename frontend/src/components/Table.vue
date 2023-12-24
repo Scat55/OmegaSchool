65b5bc7ee08badc69c4aad974d4323de5604bfd2
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-24 17:34:00 +0300

    передалал админку, но данные пока коряво отображаются

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,9 +1,71 @@
 <template>
-  <div>Таблица</div>
+  <div class="quiz-container">
+    <h2>{{ schoolName }}</h2>
+    <h3>Команда: {{ teamName }}</h3>
+
+    <div v-for="(task, index) in tasks" :key="index" class="task-container">
+      <h4>{{ task }}</h4>
+      <p class="question">Вопрос: {{ task.question }}</p>
+      <p class="answer">Правильный ответ: {{ task.correctAnswer }}</p>
+
+      <p class="user-answer">Ответ пользователя: {{ userAnswers[index] || 'Нет ответа' }}</p>
+    </div>
+
+    <p class="time">Время: {{ time }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    schoolName: String,
+    teamName: String,
+    tasks: Array,
+    correctAnswers: Array,
+    time: Array,
+    userAnswers: Array,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.quiz-container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+h1 {
+  display: block;
+}
+.task-container {
+  margin-top: 1rem;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  background-color: #fff;
+}
+
+h2,
+h3 {
+  color: #333;
+}
+
+.question,
+.answer,
+.user-answer {
+  margin: 5px 0;
+  font-size: 14px;
+}
+
+.time {
+  margin-top: 20px;
+  font-weight: bold;
+  color: #e44d26; /* цвет красного */
+}
+</style>
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="admin">
-    <div class="admin__header">
-      <div class="container">
-        <div class="admin__header-tabs">
-          <button class="admin__header-tab" @click="showTable">Проверка заданий</button>
-          <button class="admin__header-tab" @click="showTasks">Добавить задание</button>
+    <div class="admin__content">
+      <div class="admin__header">
+        <div class="container">
+          <div class="admin__header-tabs">
+            <button
+              class="admin__header-tab"
+              :class="{ 'admin__header-tab--active': table }"
+              @click="showTable"
+            >
+              Проверка заданий
+            </button>
+            <button
+              class="admin__header-tab"
+              :class="{ 'admin__header-tab--active': checkTask }"
+              @click="showTasks"
+            >
+              Добавить задание
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="admin__content">
       <div class="container">
         <div class="admin__content-table" v-if="table">
-          <Table />
+          <div class="admin__content-items">
+            <div v-for="(team, index) in teams" :key="index" class="admin__content-item">
+              <h1>Школа: {{ team.school_name }}</h1>
+              <Table
+                :teamName="team.team_name"
+                :tasks="team.task_names"
+                :correctAnswers="team.true_answer"
+                :time="team.time.map((t) => formatTime(t))"
+                :userAnswers="team.answer_comand"
+                :team="teams"
+              />
+            </div>
+          </div>
         </div>
         <div class="admin__content-tasks" v-if="checkTask">Задания</div>
       </div>
@@ -21,15 +45,39 @@
 
 <script>
 import Table from '@/components/Table.vue';
+import axios from 'axios';
+
 export default {
   components: { Table },
   data() {
     return {
       table: true,
       checkTask: false,
+      teamsPerPage: 3,
+      currentPage: 1,
+      teams: [],
     };
   },
+  computed: {
+    paginatedTeams() {
+      const startIndex = (this.currentPage - 1) * this.teamsPerPage;
+      const endIndex = startIndex + this.teamsPerPage;
+      return this.teams.slice(startIndex, endIndex);
+    },
+  },
   methods: {
+    formatTime(time) {
+      const [minutes, seconds] = time.split(':');
+      return `${parseInt(minutes, 10)} мин ${parseInt(seconds, 10)} сек`;
+    },
+    async fetchData() {
+      try {
+        const response = await axios.get('/commands/getResult/');
+        this.teams = response.data;
+      } catch (error) {
+        console.error('Ошибка при получении данных с сервера:', error);
+      }
+    },
     showTable() {
       this.table = true;
       this.checkTask = false;
@@ -38,17 +86,22 @@ export default {
       this.checkTask = true;
       this.table = false;
     },
+    handlePageClick(page) {
+      this.currentPage = page;
+    },
+  },
+  mounted() {
+    this.fetchData();
   },
 };
 </script>
-
 <style lang="scss" scoped>
 .admin {
   &__header {
-    display: flex;
-    align-items: center;
     background-color: #065dfe;
     height: 50px;
+    display: flex;
+    align-items: center;
     &-tabs {
       display: flex;
       align-items: center;
@@ -56,7 +109,6 @@ export default {
     }
     &-tab {
       font-family: 'Visitor';
-      justify-items: start;
       padding: 0.625rem;
       background-color: #168dfc;
       border: none;
@@ -64,10 +116,57 @@ export default {
       color: #fff;
       cursor: pointer;
       border-radius: 0.5rem;
+      transition: background-color 0.3s;
+      &--active {
+        background-color: #fff;
+        color: #065dfe;
+      }
+      &:hover {
+        background-color: #0e74ff;
+      }
     }
   }
-}
-.container {
-  margin: 0;
+  &__content {
+    &-table,
+    &-tasks {
+      padding: 20px;
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      margin-top: 20px;
+      background-color: #fff;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    &-items {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px;
+    }
+  }
+  &__content-item {
+    width: calc(33.33% - 20px);
+    margin-bottom: 20px;
+  }
+  &__pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    button {
+      cursor: pointer;
+      color: #065dfe;
+      border: 1px solid #ddd;
+      padding: 5px 10px;
+      border-radius: 4px;
+      transition: background-color 0.3s;
+      &:hover {
+        background-color: #f0f0f0;
+      }
+      &:disabled {
+        cursor: not-allowed;
+      }
+    }
+    span {
+      margin: 0 10px;
+    }
+  }
 }
 </style>
