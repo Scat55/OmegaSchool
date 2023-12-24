28ae0fa8f4612f89c832de758aae25045a430445
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-24 10:48:58 +0300

    Добавил каркас админки

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,11 +1,89 @@
 <template>
   <div class="admin">
-    <div class="container">Админка</div>
+    <div class="page-container">
+      <md-app md-waterfall md-mode="fixed">
+        <md-app-toolbar class="md-primary">
+          <span class="md-title">My Title</span>
+        </md-app-toolbar>
+
+        <md-app-drawer md-permanent="full">
+          <md-toolbar class="md-transparent" md-elevation="0"> Navigation </md-toolbar>
+
+          <md-list>
+            <md-list-item>
+              <md-icon>move_to_inbox</md-icon>
+              <span class="md-list-item-text">Inbox</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>send</md-icon>
+              <span class="md-list-item-text">Sent Mail</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>delete</md-icon>
+              <span class="md-list-item-text">Trash</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>error</md-icon>
+              <span class="md-list-item-text">Spam</span>
+            </md-list-item>
+          </md-list>
+          <md-icon class="fa fa-bars"></md-icon>
+        </md-app-drawer>
+
+        <md-app-content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias
+            et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea
+            repudiandae maxime? Quae non explicabo, neque.
+          </p>
+        </md-app-content>
+      </md-app>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Waterfall',
+};
 </script>
 
 <style lang="scss" scoped>
@@ -13,5 +91,18 @@ export default {};
   font-family: sans-serif;
   background-color: #fff;
   height: 100%;
+}
+.page-container {
+  height: 100%;
+}
+.md-app {
+  height: 100%;
+  border: 1px solid rgba(#000, 0.12);
+}
+
+// Demo purposes only
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
 }
 </style>
